b5a98429294875f6089f8ae90773d92ccd821fd9
Michalju <juszkin@gmail.com>
2020-11-20 22:49:25 +0000

    tidy upo result page and added forms to choose name and choice

--- app/controller.py
+++ app/controller.py
@@ -11,8 +11,22 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/play_game')
+def play_game():
+    return render_template('play_game.html')
+
+@app.route('/game_result', methods=["POST"])
+def game_result():
+    form_data = request.form 
+ 
+    player_1 = Player (form_data['player_1_name'], form_data['player_1_choice'])
+    player_2 = Player (form_data['player_2_name'], form_data['player_2_choice'])
+    r_p_s = Game("Rock Paper Scissors", player_1, player_2)
+    r_p_s.play()
+    return render_template('result.html', game = r_p_s)
+
 @app.route('/<player_1_choice>/<player_2_choice>')
-def play_game(player_1_choice,player_2_choice):
+def test_game_result(player_1_choice,player_2_choice):
     player_1 = Player ("Boris the blade", player_1_choice)
     player_2 = Player ("Batman", player_2_choice)
     r_p_s = Game("Rock Paper Scissors", player_1, player_2)
