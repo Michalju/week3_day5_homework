f2b9ee89af2c490e7999387e697a3351c691afe4
Michalju <juszkin@gmail.com>
2020-11-20 16:44:26 +0000

    added method play to class Game

--- app/controller.py
+++ app/controller.py
@@ -5,11 +5,10 @@ from app.models.player import Player
 
 
 
-@app.route('/<var1>/<var2>')
-def index(var1,var2):
-    player_1 = Player ("Boris the blade", var1)
-    player_2 = Player ("Batman", var2)
-    rock_paper_scissors = Game("Rock Paper Scissors", player_1, player_2)
-
-    return render_template('index.html', title = rock_paper_scissors.play())
-
+@app.route('/<player_1_choice>/<player_2_choice>')
+def index(player_1_choice,player_2_choice):
+    player_1 = Player ("Boris the blade", player_1_choice)
+    player_2 = Player ("Batman", player_2_choice)
+    r_p_s = Game("Rock Paper Scissors", player_1, player_2)
+    r_p_s.play()
+    return render_template('result.html', game = r_p_s)
--- app/models/game.py
+++ app/models/game.py
@@ -3,6 +3,24 @@ class Game():
         self.name = name
         self.player_1 = player_1
         self.player_2 = player_2
+        self._result = ''
 
     def play(self):
-        return f"{self.player_1.name} won the game"
+        # Rock Paper Scissor rules
+        # Rock (1) beats Scissors(2)
+        # Scissors(2) beat Paper (3)
+        # Paper (3) beat Rock(1)
+        
+        # Below is when there is a tie 
+        if self.player_1.choice == self.player_2.choice:
+            self._result = "None won the game"            
+
+        # Below is when Player 1 has rock and wins    
+        elif(self.player_1.choice == "rock" and self.player_2.choice == "scissors") or \
+            (self.player_1.choice == "scissors" and self.player_2.choice == "paper") or \
+            (self.player_1.choice == "paper" and self.player_2.choice == "rock"):
+            self._result =  f"{self.player_1.name} won the game"
+
+        # otherwise Player 2 wins
+        else:         
+            self._result =  f"{self.player_2.name} won the game"
